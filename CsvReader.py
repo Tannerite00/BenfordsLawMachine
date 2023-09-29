8b7dfaf1979ee403d2f0d8acfda53811bb023c6e
Tanner Dodd <31601567+Tannerite00@users.noreply.github.com>
2023-09-29 14:54:27 -0600

    Update CsvReader.py
    added functionality so users can choose what columns they want to run analysis on, allowing users to search for files without having to modify them to fit the old criteria.

--- CsvReader.py
+++ CsvReader.py
@@ -15,24 +15,130 @@ digit_counts = {str(i): 0 for i in range(1, 10)}
 
 # Initialize a variable to count the total number of digits
 total_digit_count = 0
+# Create the main application window
+root = tk.Tk()
+root.title("CSV File Processor")
+
+# Initialize Tkinter variables for selected column names as globals
+var_col1 = tk.StringVar()
+var_col2 = tk.StringVar()
+var_col3 = tk.StringVar()
 
 # Function to handle the file selection
 def browse_file():
+    global var_col1, var_col2, var_col3  # Declare them as global
     file_path = filedialog.askopenfilename(filetypes=[("CSV Files", "*.csv")])
     if file_path:
         entry_file_path.delete(0, tk.END)  # Clear the entry field
         entry_file_path.insert(0, file_path)  # Set the selected file path
 
+        # Get the list of columns from the CSV file
+        with open(file_path, newline='') as csvfile:
+            csvreader = csv.reader(csvfile)
+            header = next(csvreader)  # Read the header row
+            column_names = header  # Get column names from the header row
+
+        # Call a function to handle the retrieved column names
+        handle_column_names(column_names)
+
+def handle_column_names(column_names):
+    global var_col1, var_col2, var_col3  # Access the global variables
+
+    # Create dropdown menus for each column
+    dropdown_menu1 = tk.OptionMenu(root, var_col1, *column_names)
+    dropdown_menu2 = tk.OptionMenu(root, var_col2, *column_names)
+    dropdown_menu3 = tk.OptionMenu(root, var_col3, *column_names)
+
+    # Label each dropdown menu
+    label_menu1 = tk.Label(root, text="Account Description:")
+    label_menu2 = tk.Label(root, text="Transaction Description:")
+    label_menu3 = tk.Label(root, text="Transactions:")
+    
+
+    # Grid layout for labels and dropdown menus
+    label_menu1.grid(row=5, column=0, padx=10, pady=5)
+    dropdown_menu1.grid(row=5, column=1, padx=10, pady=5)
+    label_menu2.grid(row=6, column=0, padx=10, pady=5)
+    dropdown_menu2.grid(row=6, column=1, padx=10, pady=5)
+    label_menu3.grid(row=7, column=0, padx=10, pady=5)
+    dropdown_menu3.grid(row=7, column=1, padx=10, pady=5)
+
+
 # Define a function to clean and convert 'Total' values to float
-def clean_and_convert_total(total_str):
-    total_str = total_str.replace('$', '').replace(',', '').strip()
-    # Handle negative numbers enclosed in parentheses
-    if total_str.startswith('(') and total_str.endswith(')'):
-        total_str = '-' + total_str[1:-1]
-    try:
-        return float(total_str)
-    except ValueError:
+def clean_and_convert_total(total_str_or_float):
+    if isinstance(total_str_or_float, str):
+        total_str = total_str_or_float.replace('$', '').replace(',', '').strip()
+        # Handle negative numbers enclosed in parentheses
+        if total_str.startswith('(') and total_str.endswith(')'):
+            total_str = '-' + total_str[1:-1]
+        try:
+            return float(total_str)
+        except ValueError:
+            return np.nan
+    elif isinstance(total_str_or_float, float):
+        # If it's already a float, return it as is
+        return total_str_or_float
+    else:
+        # If it's neither a string nor a float, return NaN
         return np.nan
+
+
+def process_gaussian_distribution(csv_file_path, col1, col3):
+    try:
+        # Load transaction data from the selected CSV file
+        data = pd.read_csv(csv_file_path)
+        col1 = var_col1.get()
+        col3 = var_col2.get()
+
+        # Clean and convert the entire col3 column to float
+        data[col3] = data[col3].apply(clean_and_convert_total)
+
+        # Group transactions by 'Item'
+        grouped_data = data.groupby([col1])
+        
+        # Plot Gaussian distributions for each selected column
+        for i, group_data in grouped_data:
+            # Fit a normal distribution to the data for the current column
+            mu, std = norm.fit(group_data[col3])
+
+            # Create a histogram
+            plt.figure(figsize=(10, 6))
+            plt.hist(group_data[col3], bins=30, density=True, alpha=0.6, color='blue')
+
+            # Create a range of values for the x-axis based on the normal distribution
+            xmin, xmax = plt.xlim()
+            x = np.linspace(xmin, xmax, 100)
+            p = norm.pdf(x, mu, std)
+
+            # Plot the fitted normal distribution
+            plt.plot(x, p, 'k', linewidth=2)
+            title = f"Fit results for {i}: mu = {mu:.2f}, std = {std:.2f}"
+            plt.title(title)
+
+            plt.xlabel('Dollars Spent (Total)')
+            plt.ylabel('')
+
+            # Calculate and plot the first standard deviation lines
+            std1_left = mu - std
+            std1_right = mu + std
+            plt.axvline(x=std1_left, color='k', linestyle='--', label='1st std dev', linewidth=2)
+            plt.axvline(x=std1_right, color='k', linestyle='--', linewidth=2)
+
+            # Calculate and plot the second standard deviation lines
+            std2_left = mu - 2 * std
+            std2_right = mu + 2 * std
+            plt.axvline(x=std2_left, color='r', linestyle='--', label='2nd std dev', linewidth=2)
+            plt.axvline(x=std2_right, color='r', linestyle='--', linewidth=2)
+
+            # Force y-axis ticks to display whole numbers
+            yint = range(min([int(val) for val in plt.yticks()[0]]), max([int(val) for val in plt.yticks()[0]]) + 1)
+            plt.yticks(yint)
+            plt.show()
+    except ValueError as e:
+        print(f"Error processing data for item {i}: {e}")
+            
+    except FileNotFoundError:
+        print("CSV file not found. Please select a valid CSV file.")
 
 # Function to process the selected CSV file and run Benford's Law analysis
 def process_benfords_law():
@@ -90,74 +196,20 @@ def process_benfords_law():
         plt.legend()
         plt.show()
 
-# Function to run Gaussian distribution analysis
-def process_gaussian_distribution(csv_file_path):
-    try:
-        # Load transaction data from the selected CSV file
-        data = pd.read_csv(csv_file_path)
-
-        # Clean and convert 'Total' column values
-        data['Total'] = data['Total'].apply(clean_and_convert_total)
-
-        # Group transactions by 'Item'
-        grouped_data = data.groupby('Item')
-
-        # Plot Gaussian distributions for each group
-        for item, group_data in grouped_data:
-            # Fit a normal distribution to the data for the current item group
-            mu, std = norm.fit(group_data['Total'])
-
-            # Create a histogram
-            plt.figure(figsize=(10, 6))
-            plt.hist(group_data['Total'], bins=30, density=True, alpha=0.6, color='blue')
-
-            # Create a range of values for the x-axis based on the normal distribution
-            xmin, xmax = plt.xlim()
-            x = np.linspace(xmin, xmax, 100)
-            p = norm.pdf(x, mu, std)
-
-            # Plot the fitted normal distribution
-            plt.plot(x, p, 'k', linewidth=2)
-            title = f"Fit results for {item}: mu = {mu:.2f}, std = {std:.2f}"
-            plt.title(title)
-
-            plt.xlabel('Dollars Spent (Total)')
-            plt.ylabel('')
-
-            # Calculate and plot the first standard deviation lines
-            std1_left = mu - std
-            std1_right = mu + std
-            plt.axvline(x=std1_left, color='k', linestyle='--', label='1st std dev', linewidth=2)
-            plt.axvline(x=std1_right, color='k', linestyle='--', linewidth=2)
-
-            # Calculate and plot the second standard deviation lines
-            std2_left = mu - 2 * std
-            std2_right = mu + 2 * std
-            plt.axvline(x=std2_left, color='r', linestyle='--', label='2nd std dev', linewidth=2)
-            plt.axvline(x=std2_right, color='r', linestyle='--', linewidth=2)
-
-
-            # Force y-axis ticks to display whole numbers
-            yint = range(min([int(val) for val in plt.yticks()[0]]), max([int(val) for val in plt.yticks()[0]]) + 1)
-            plt.yticks(yint)
-            plt.show()
-    except ValueError as e:
-        print(f"Error processing data for item {item}: {e}")
-            
-    except FileNotFoundError:
-        print("CSV file not found. Please select a valid CSV file.")
-
 # Function to run transaction analysis 
-def analyze_transactions_from_csv(csv_file_path):
+def analyze_transactions_from_csv(csv_file_path, col1, col2, col3):
     try:
         # Load transaction data from the selected CSV file
         data = pd.read_csv(csv_file_path)
+        col1 = var_col1.get()
+        col2 = var_col2.get()
+        col3 = var_col3.get()
 
         # Clean and convert 'Total' column values
-        data['Total'] = data['Total'].apply(clean_and_convert_total)
+        data[col3] = data[col3].apply(clean_and_convert_total)
 
-          # Create an interactive horizontal histogram using Plotly
-        fig = px.bar(data, x='Total', y='Rep', color='Item', orientation='h',
+        # Create an interactive horizontal histogram using Plotly
+        fig = px.bar(data, x=data[col3], y=data[col2], color=data[col1], orientation='h',
                      title='Total Spending by Rep',
                      labels={'Total': 'Dollars Spent (Total)', 'Rep': 'Rep'})
 
@@ -242,31 +294,30 @@ def display_data_as_modal(data):
 
     modal.mainloop()
 
-# Create the main application window
-root = tk.Tk()
-root.title("CSV File Processor")
 
 # Create and configure widgets
 label_instruction = tk.Label(root, text="Select a CSV file:")
 entry_file_path = tk.Entry(root, width=40)
 button_browse = tk.Button(root, text="Browse", command=browse_file)
 button_process_benfords_law = tk.Button(root, text="Run Benford's Law", command=process_benfords_law)
-button_process_gaussian = tk.Button(root, text="Run Gaussian Distribution", command=lambda: process_gaussian_distribution(entry_file_path.get()))
-button_process_transaction_analysis = tk.Button(root, text="Run Transaction Analysis", command=lambda: analyze_transactions_from_csv(entry_file_path.get()))
+button_process_gaussian = tk.Button(root, text="Run Gaussian Distribution", command=lambda: process_gaussian_distribution(entry_file_path.get(), var_col1.get(), var_col3.get()))
+button_process_transaction_analysis = tk.Button(root, text="Run Transaction Analysis", command=lambda: analyze_transactions_from_csv(entry_file_path.get(), var_col1.get(), var_col2.get(), var_col3.get()))
 button_show_first_10_rows = tk.Button(root, text="Show First 10 Rows", command=lambda: show_first_10_rows_as_excel(entry_file_path.get()))
 button_find_duplicate_rows = tk.Button(root, text="Find Duplicates", command=lambda: find_duplicate_rows(entry_file_path.get()))
 
-# Arrange widgets in the layout
-label_instruction.pack()
-entry_file_path.pack()
-button_browse.pack()
-button_process_benfords_law.pack()
-button_process_gaussian.pack()
-button_process_transaction_analysis.pack()
-button_find_duplicate_rows.pack()
-button_show_first_10_rows.pack()
+# Arrange widgets in the layout using grid
+label_instruction.grid(row=0, column=0, columnspan=2, pady=10)
+entry_file_path.grid(row=1, column=0, columnspan=2, padx=10, pady=5)
+button_browse.grid(row=1, column=2, padx=10, pady=5)
+button_process_benfords_law.grid(row=3, column=0, padx=10, pady=5)
+button_process_gaussian.grid(row=3, column=1, padx=10, pady=5)
+button_process_transaction_analysis.grid(row=3, column=2, padx=10, pady=5)
+button_find_duplicate_rows.grid(row=4, column=0, columnspan=2, padx=10, pady=5)
+button_show_first_10_rows.grid(row=4, column=2, padx=10, pady=5)
+
 
 # Start the Tkinter main loop
 root.mainloop()
+
 
 
