19380806471b556ba9057ef645fc140cc322e267
Tanner Dodd <31601567+Tannerite00@users.noreply.github.com>
2023-10-17 08:37:58 -0600

    Update CsvReader.py

--- CsvReader.py
+++ CsvReader.py
@@ -390,17 +390,26 @@ def show_excel_file(csv_file_path):
 def open_info_dialog():
     # Explanation text
     explanation_text = """
-    Benford's Law Analysis:    
+    Benford's Law Analysis:
+    
         A simple python script that takes a CSV and applies Benford's Law to it, where in any financial data typically the leading number 1 appears approx. 30 percent of the time, with a sloping distribution down to 9 that should appear approx. 5 percent of the time.    
         The basic theory is that when someone commits fraud, they do not do it in increments of $10, $20, $100, or $200, instead they do it in $80 or $900 increments. Therefor, if the higher numbers appear more often than the large number average distribution, there is a chance fraud is at play.    
+   
     Gaussian Distribution Analysis:
+       
         Gives a normal distribution based on the total spent on each Item by each Rep. If there is an abnormally large or small transaction it is made clearly visible for the user to see. WIP: Plots a normal distribution for each item group, with first and second std dev lines plotted to easily pick out outliers.    
         Select 3 columns from the drop down menus, an account description (or item category), the transaction description (what item/service was purchased) and the transaction column (total spent on item).
+   
     Transaction Analysis:
+    
      A transctional analytics tool that takes 3 variables from a csv: Which Rep bought an item, what Item was bought, and the Total amount spent on an item. if a person bought the same item multiple times, the graph will sum them but also show a partition to display the different transactions. The graph is interactive, so specific items can be viewed together with others or on their own, and the graph can be zoomed in and out to see all parts of the data or just specific parts the user wants to focus on.
         Select 3 columns from the drop down menus, an account description (or item category), the transaction description (what item/service was purchased) and the transaction column (total spent on item).
+    
     Finding Duplicates:
         Finds duplicate transactions in the data based off of if the 3 columns chosen by the user match with any of the other data, renders it in an excel-like grid in a seperate modal and saves them to a new csv.
+   
+    Field Statistics:
+        Automatic stats that run when the file is selected such as: number of records, largest value, smallest value.
     """
     messagebox.showinfo("Analysis Explanations", explanation_text)
 
